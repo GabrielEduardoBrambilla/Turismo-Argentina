a9102311abd54772a426d811d6f6d58bb059fdcf
Brambs <gabriel128.br@gmail.com>
2024-09-26 17:55:13 -0300

    Try to config tailwind prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,29 @@ import {
   CarouselContent,
   CarouselItem,
   CarouselNext,
-  CarouselPrevious
+  CarouselPrevious,
 } from '@/components/ui/carousel'
+import Autoplay from 'embla-carousel-autoplay'
+import Image from 'next/image'
+import Link from 'next/link'
+import Marquee from 'react-fast-marquee'
 import img1 from '../assets/imgs/1a50c6f9-c42d-438c-8ca6-6a7ef59e4c16.jpeg'
 import img2 from '../assets/imgs/498b328c-bacf-4595-8a10-0fca8cbd6b8a.jpeg'
 import img3 from '../assets/imgs/97913c2a-c3c9-462f-b859-b52cf4e42ffb.jpeg'
-import Image from 'next/image'
-import Autoplay from 'embla-carousel-autoplay'
-import Marquee from 'react-fast-marquee'
-import Link from 'next/link'
 
 export default function page() {
   return (
     <>
-      <div className="Carosel relative w-full p-  bg-blue-500">
+      <div className="Carosel relative w-full p- bg-blue-500">
         <Carousel
           orientation="horizontal"
           plugins={[
             Autoplay({
-              delay: 4000
-            })
+              delay: 4000,
+            }),
           ]}
           opts={{
-            loop: true
+            loop: true,
           }}
         >
           <CarouselContent className="flex h-lvh">
@@ -57,8 +57,8 @@ export default function page() {
         <h2 className="text-4xl my-8 font-bold">Conoce nuestros tours</h2>
 
         <div className="grid grid-cols-3 gap-12">
-          <Link href={''} about="">
-            <div className=" card relative flex content-center overflow-hidden h-[32rem] text-white justify-items-end items-end rounded-3xl ">
+          <Link href={'./viajens/first'} about="">
+            <div className="content-center flex card relative overflow-hidden h-[32rem] text-white justify-items-end items-end rounded-3xl ">
               <Image
                 alt=""
                 src={img2}
@@ -68,7 +68,7 @@ export default function page() {
                 Nacional
               </p>
 
-              <div className="content p-4 h-1/2 w-full  bg-gradient-to-t from-black to-transparent flex flex-col justify-center flex-wrap gap-6 rounded-b-lg ">
+              <div className="content p-4 h-1/2 bg-gradient-to-t w-full   from-black to-transparent flex flex-col justify-center flex-wrap gap-6 rounded-b-lg ">
                 <p className="font-bold w-fit text-3xl mt-6">
                   Carretara Austral
                 </p>
---
+++ src/app/viajens/first/page.tsx
@@ -0,0 +1,10 @@
+import img2 from '@/assets/imgs/ec322157-44d3-457c-b1d9-bf5446cc9005.jpeg'
+import Image from 'next/image'
+
+export default function TripPage() {
+  return (
+    <section className="flex-col flex p-0">
+      <Image alt="" src={img2} />
+    </section>
+  )
+}
